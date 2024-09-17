bbfa46b4692f68dea9b1e7218842e88d86ba95df
Azat Nasyrov <azat.nasyrov31@gmail.com>
2024-09-17 09:20:56 +0600

    added connection to the db

--- apps/notifications-microservice/src/notifications-microservice.module.ts
+++ apps/notifications-microservice/src/notifications-microservice.module.ts
@@ -1,9 +1,21 @@
 import { Module } from '@nestjs/common';
 import { NotificationsMicroserviceController } from './notifications-microservice.controller';
 import { NotificationsMicroserviceService } from './notifications-microservice.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.POSTGRES_HOST,
+      port: +process.env.POSTGRES_PORT,
+      username: process.env.POSTGRES_USER,
+      password: process.env.POSTGRES_PASSWORD,
+      database: process.env.POSTGRES_DB,
+      autoLoadEntities: true,
+      synchronize: false,
+    }),
+  ],
   controllers: [NotificationsMicroserviceController],
   providers: [NotificationsMicroserviceService],
 })
--- apps/orders-microservice/src/app.module.ts
+++ apps/orders-microservice/src/app.module.ts
@@ -1,9 +1,21 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.POSTGRES_HOST,
+      port: +process.env.POSTGRES_PORT,
+      username: process.env.POSTGRES_USER,
+      password: process.env.POSTGRES_PASSWORD,
+      database: process.env.POSTGRES_DB,
+      autoLoadEntities: true,
+      synchronize: false,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
